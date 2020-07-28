c2c25d73255b8255e4237dff7aa4d2cef2fbb11a
Andre Weltsch <andre@weltsch.de>
2020-07-28 07:22:51 +0200

    Simple QWERTY layout for andreus_pcb.

--- keyboards/handwired/andreus_pcb/keymaps/default/keymap.c
+++ keyboards/handwired/andreus_pcb/keymaps/default/keymap.c
@@ -4,9 +4,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {
-    { KC_A, KC_B, KC_C, KC_D, KC_E, KC_F, KC_G, KC_H, KC_I, KC_J },
-    { KC_K, KC_L, KC_M, KC_N, KC_O, KC_P, KC_Q, KC_R, KC_S, KC_T },
-    { KC_U, KC_V, KC_W, KC_X, KC_Y, KC_Z, KC_1, KC_2, KC_3, KC_4 },
-    { KC_NO, KC_NO, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_NO, KC_NO },
+    { KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P },
+    { KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN },
+    { KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH },
+    { KC_NO, KC_NO, KC_0, KC_TAB, KC_ENT, KC_BSPC, KC_SPC, KC_1, KC_NO, KC_NO },
   }
 };
