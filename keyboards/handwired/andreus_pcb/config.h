da52c62acbedeb73533780f2f45dcf8204a793be
Andre Weltsch <andre@weltsch.de>
2020-07-28 07:16:06 +0200

    Reassign layout pins on andreus_pcb.

--- keyboards/handwired/andreus_pcb/config.h
+++ keyboards/handwired/andreus_pcb/config.h
@@ -34,9 +34,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Planck PCB default pin-out */
 //#define MATRIX_ROW_PINS { F4, F5, F6, F7 }
-#define MATRIX_ROW_PINS { F7, F6, F5, F4 }
+// #define MATRIX_ROW_PINS { F7, F6, F5, F4 }
+#define MATRIX_ROW_PINS { C6, D4, D0, D1 }
 //#define MATRIX_COL_PINS { D1, D0, D4, C6, D7, B1, B3, B2, B6, E6 }
-#define MATRIX_COL_PINS { D7, C6, D4, D0, D1, B6, B2, B3, B1, E6 }
+// #define MATRIX_COL_PINS { D7, C6, D4, D0, D1, B6, B2, B3, B1, E6 }
+#define MATRIX_COL_PINS { B1, F7, F6, F5, F4, B5, B4, E6, D7, B3 }
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
